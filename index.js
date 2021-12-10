fb2f7a7873267d389ed68805f960e27cf265a855
Rubber T. Dork <hi@rubberdork.com>
2021-12-10 13:59:52 +0800

    Scaffold discord bot

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+import { Client, Intents } from 'discord.js'
+
 
 const {
   NODE_ENV,
@@ -8,3 +10,18 @@ const {
   DISCORD_TOKEN,
   DISCORD_GUILD_ID,
 } = process.env
+
+const bot = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ]
+})
+
+bot.once('ready', () => console.log(`Tipbot logged in as ${DISCORD_CLIENT_ID}`))
+bot.on('messageCreate', async (msg) => {
+  const { channelId, content, author } = msg
+  if (author.id === DISCORD_CLIENT_ID) return
+  console.log(msg)
+})
+bot.login(DISCORD_TOKEN)
