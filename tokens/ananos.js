5feb8ca69984709bdd8ed51e5532271bfc6b9844
Rubber T. Dork <hi@rubberdork.com>
2021-12-13 16:01:38 +0800

    Add issuer & logos token properties

--- tokens/ananos.js
+++ tokens/ananos.js
@@ -1,6 +1,11 @@
 export const Ananos = {
   name: 'Ananos',
   code: 'Ananos',
+  issuer: '',
+  logo: {
+    emoji: '<:ananos:919818423461109810>',
+    image: ''
+  },
   aliases: [
     'ananos',
     'ana',
--- tokens/dorkbucks.js
+++ tokens/dorkbucks.js
@@ -1,6 +1,11 @@
 export const DorkBucks = {
   name: 'DorkBucks',
   code: 'DORK',
+  issuer: '',
+  logo: {
+    emoji: '<:rupert:912875178860621836>',
+    image: ''
+  },
   aliases: [
     'dorkbucks',
     'dork',
