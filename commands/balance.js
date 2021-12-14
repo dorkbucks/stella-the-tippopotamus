df98287bb6a6d1dded6c8b581adb5f4ade3e593c
Rubber T. Dork <hi@rubberdork.com>
2021-12-14 10:31:36 +0800

    Use new account data in .bal

--- commands/balance.js
+++ commands/balance.js
@@ -5,14 +5,16 @@ import { tokens } from '../tokens/index.js'
 const TOKENS = tokens.list()
 
 export class Balance {
-  constructor (fromID) {
-    this.fromID = fromID
+  constructor (account) {
+    this.account = account
   }
 
   async call () {
-    const { balances } = await Account.getOrCreate(this.fromID, TOKENS)
+    const { username, avatar, balances } = await Account.getOrCreate(this.account, TOKENS)
     return {
       message: {
+        heading: `${username}'s balances`,
+        icon: avatar,
         body: Object.keys(balances).sort((a, b) => a.localeCompare(b)).reduce((str, name) => {
           const { emoji } = tokens.get(name, 'logo')
           const bal = (+balances[name]).toLocaleString()
