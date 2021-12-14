fe06bc33fcaa09249bc61bdd6bcd3d9943e72d8a
Rubber T. Dork <hi@rubberdork.com>
2021-12-14 20:43:34 +0800

    Add Balance tests

---
+++ src/commands/balance.test.js
@@ -0,0 +1,29 @@
+import { test } from 'tap'
+import BigNumber from 'bignumber.js'
+
+import { tokens } from '../tokens/index.js'
+import { Account } from '../lib/account.js'
+import { Balance } from './balance.js'
+
+
+const TOKENS = tokens.list('name')
+
+test('Balance', (t) => {
+  const account = new Account({
+    _id: '1234',
+    username: 'rubberdork',
+    avatar: 'avatar.png',
+    balances: TOKENS.reduce((o, token) => {
+      o[token.name] = { ...BigNumber(10000.01) }
+      return o
+    }, {})
+  }, TOKENS)
+
+  const bal = new Balance(account)
+  const { message: { heading, icon, body } } = bal.call()
+  t.equal(heading, `${account.username}'s balances`)
+  t.equal(icon, account.avatar)
+  const match = body.match(/10,000.01/g)
+  t.equal(match.length, TOKENS.length)
+  t.end()
+})
