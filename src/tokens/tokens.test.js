14b76fb391c462545136637ad1839355082c49f2
Rubber T. Dork <hi@rubberdork.com>
2021-12-15 16:52:46 +0800

    Wrap tokens test in suite

--- src/tokens/tokens.test.js
+++ src/tokens/tokens.test.js
@@ -1,20 +1,24 @@
-import tap from 'tap'
+import { test } from 'tap'
 
 import { tokens } from './index.js'
 
 
-const tokenList = tokens.list('name', 'logo', 'issuer')
-tokenList.forEach((token) => {
-  const keys = Object.keys(token)
-  tap.equal(keys.length, 3)
-  tap.ok(token.hasOwnProperty('name'))
-  tap.ok(token.hasOwnProperty('logo'))
-  tap.ok(token.hasOwnProperty('issuer'))
-})
+test('.list()', (t) => {
+  const tokenList = tokens.list('name', 'logo', 'issuer')
+  tokenList.forEach((token) => {
+    const keys = Object.keys(token)
+    t.equal(keys.length, 3)
+    t.ok(token.hasOwnProperty('name'))
+    t.ok(token.hasOwnProperty('logo'))
+    t.ok(token.hasOwnProperty('issuer'))
+  })
 
-const tokenList2 = tokens.list()
-tokenList2.forEach((token) => {
-  const keys = Object.keys(token)
-  tap.equal(keys.length, 1)
-  tap.ok(token.hasOwnProperty('name'), 'Defaults to "name" prop')
+  const tokenList2 = tokens.list()
+  tokenList2.forEach((token) => {
+    const keys = Object.keys(token)
+    t.equal(keys.length, 1)
+    t.ok(token.hasOwnProperty('name'), 'Defaults to "name" prop')
+  })
+
+  t.end()
 })
