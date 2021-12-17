dbfdf2ef457e022133fd21260464d328f8bebf1c
Rubber T. Dork <hi@rubberdork.com>
2021-12-17 17:38:39 +0800

    Add Intent & partials needed to receive DMs

--- src/lib/bot.js
+++ src/lib/bot.js
@@ -7,7 +7,12 @@ import { Client, Intents } from 'discord.js'
 export const bot = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES
+  ],
+  partials: [
+    // Need this to receive DMs
+    'CHANNEL'
   ]
 })
 bot.token = process.env.DISCORD_TOKEN
