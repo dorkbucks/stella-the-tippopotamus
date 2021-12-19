6327433fc9a1d990896cdc2829d94469eb81ac25
Rubber T. Dork <hi@rubberdork.com>
2021-12-19 12:12:53 +0800

    Export walletAddress from wallet module

--- src/commands/deposit.js
+++ src/commands/deposit.js
@@ -1,8 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import QRCode from 'qrcode'
 
+import { walletAddress } from '../wallet/index.js'
 import { tokens } from '../tokens/index.js'
 
 
@@ -10,21 +8,20 @@ const tokenList = tokens.list('name', 'logo').reduce((str, { name, logo }) => {
   str = `${str}\n${logo.emoji} **${name}**`
   return str
 }, '')
-const pk = process.env.ACCOUNT_PUBLIC_KEY
 
 export class DepositRequest {
   static channelTypes = ['DM']
 
   async call (sender) {
     const memo = sender._id
-    const url = `web+stellar:pay?destination=${pk}&memo=${memo}`
+    const url = `web+stellar:pay?destination=${walletAddress}&memo=${memo}`
     const qrCodeDataURI = await QRCode.toDataURL(url)
     const qrCodeBuffer = new Buffer.from(qrCodeDataURI.split(',')[1], 'base64')
 
     return {
       message: {
         heading: 'You may deposit any of these supported tokens:',
-        body: `${tokenList}\n\nScan the qr code with your wallet or send to:\n **${pk}**\n with memo: \n **${memo}**`,
+        body: `${tokenList}\n\nScan the qr code with your wallet or send to:\n **${walletAddress}**\n with memo: \n **${memo}**`,
         image: { name: 'qrcode.png', attachment: qrCodeBuffer }
       }
     }
--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,1 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 export { startDepositWatcher } from './deposit_watcher.js'
+export const walletAddress = process.env.ACCOUNT_PUBLIC_KEY
