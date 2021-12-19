6fe78ea5d48e2b6bd95e8af5d08df044536a1c82
Rubber T. Dork <hi@rubberdork.com>
2021-12-19 15:58:42 +0800

    Wrap withdraw #parseArgs tests in suite

--- src/commands/withdraw.test.js
+++ src/commands/withdraw.test.js
@@ -1,4 +1,4 @@
-import tap from 'tap'
+import { test } from 'tap'
 
 import { tokens } from '../tokens/index.js'
 import { Account } from '../lib/account.js'
@@ -14,36 +14,40 @@ function createFundedAccount () {
 }
 
 const TOKENS = tokens.list()
-const sender = createFundedAccount()
-const address = `GC6SOPXA7X7LDKJK3SDHL6MQEQLOHF23G5CN2MLT4MJ2UPFUSKRKIURG`
-let withdrawal = new WithdrawalRequest()
 
-var parsedArgs = withdrawal.parseArgs(['100', 'dork', address])
-tap.equal(parsedArgs.amount, 100)
-tap.equal(parsedArgs.token, 'dork')
-tap.equal(parsedArgs.address, address)
+test('parseArgs', (t) => {
+  const sender = createFundedAccount()
+  const address = `GC6SOPXA7X7LDKJK3SDHL6MQEQLOHF23G5CN2MLT4MJ2UPFUSKRKIURG`
+  let withdrawal = new WithdrawalRequest()
 
-var parsedArgs = withdrawal.parseArgs(['100k', 'dork', address])
-tap.equal(parsedArgs.amount, 100000)
-tap.equal(parsedArgs.token, 'dork')
-tap.equal(parsedArgs.address, address)
+  var parsedArgs = withdrawal.parseArgs(['100', 'dork', address])
+  t.equal(parsedArgs.amount, 100)
+  t.equal(parsedArgs.token, 'dork')
+  t.equal(parsedArgs.address, address)
 
-var parsedArgs = withdrawal.parseArgs(['all', 'dork', address])
-tap.equal(parsedArgs.amount, 'all')
-tap.equal(parsedArgs.token, 'dork')
-tap.equal(parsedArgs.address, address)
+  var parsedArgs = withdrawal.parseArgs(['100k', 'dork', address])
+  t.equal(parsedArgs.amount, 100000)
+  t.equal(parsedArgs.token, 'dork')
+  t.equal(parsedArgs.address, address)
 
-var parsedArgs = withdrawal.parseArgs(['dork', '100', address])
-tap.equal(parsedArgs, null)
+  var parsedArgs = withdrawal.parseArgs(['all', 'dork', address])
+  t.equal(parsedArgs.amount, 'all')
+  t.equal(parsedArgs.token, 'dork')
+  t.equal(parsedArgs.address, address)
 
-var parsedArgs = withdrawal.parseArgs(['100', 'dork', address, 'memo message'])
-tap.equal(parsedArgs.amount, 100)
-tap.equal(parsedArgs.token, 'dork')
-tap.equal(parsedArgs.address, address)
-tap.equal(parsedArgs.memo, 'memo message')
+  var parsedArgs = withdrawal.parseArgs(['dork', '100', address])
+  t.equal(parsedArgs, null)
 
-var parsedArgs = withdrawal.parseArgs(['100', 'dork', address, '100230'])
-tap.equal(parsedArgs.amount, 100)
-tap.equal(parsedArgs.token, 'dork')
-tap.equal(parsedArgs.address, address)
-tap.equal(parsedArgs.memo, '100230')
+  var parsedArgs = withdrawal.parseArgs(['100', 'dork', address, 'memo message'])
+  t.equal(parsedArgs.amount, 100)
+  t.equal(parsedArgs.token, 'dork')
+  t.equal(parsedArgs.address, address)
+  t.equal(parsedArgs.memo, 'memo message')
+
+  var parsedArgs = withdrawal.parseArgs(['100', 'dork', address, '100230'])
+  t.equal(parsedArgs.amount, 100)
+  t.equal(parsedArgs.token, 'dork')
+  t.equal(parsedArgs.address, address)
+  t.equal(parsedArgs.memo, '100230')
+  t.end()
+})
