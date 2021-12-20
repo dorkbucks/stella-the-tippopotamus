632801ca7b657036cf7e75c7fc5784d7ddb7bb2b
Rubber T. Dork <hi@rubberdork.com>
2021-12-20 16:54:56 +0800

    Export wallet keypair

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+import { Keypair } from 'stellar-sdk'
+
 
 export { startDepositWatcher } from './deposit_watcher.js'
 export const walletAddress = process.env.ACCOUNT_PUBLIC_KEY
+export const walletKeypair = Keypair.fromSecret(process.env.ACCOUNT_SECRET_KEY)
