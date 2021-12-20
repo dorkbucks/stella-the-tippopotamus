20d92566794b131bf57631093eb8aaf215924edc
Rubber T. Dork <hi@rubberdork.com>
2021-12-20 16:54:39 +0800

    Add stellar.expert txn link builder

--- src/stellar/index.js
+++ src/stellar/index.js
@@ -10,7 +10,9 @@ const testnet = NODE_ENV === 'development'
 const NETWORK = testnet ? 'TESTNET' : 'PUBLIC'
 const networkPassphrase = Networks[NETWORK]
 const HORIZON_URL = `https://horizon${testnet ? '-testnet' : ''}.stellar.org`
+const EXPERT_BASE_URL = `https://stellar.expert/explorer/${testnet ? 'testnet' : 'public'}`
 
+export const expertTxnURL = (type, id) => `${EXPERT_BASE_URL}/tx/${id}`
 export const server = new Server(HORIZON_URL)
 export const txnOpts = {
   fee: STELLAR_MAX_FEE,
