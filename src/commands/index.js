86777ebbab83a895b6305dfdb14ce49f35bda0af
Rubber T. Dork <hi@rubberdork.com>
2021-12-24 15:06:46 +0800

    Add .whos active command

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,6 +2,7 @@ import { Tip } from './tip.js'
 import { Balance } from './balance.js'
 import { DepositRequest } from './deposit.js'
 import { WithdrawalRequest } from './withdraw.js'
+import { Whos } from './whos.js'
 
 
 export const commands = new Map()
@@ -9,6 +10,7 @@ commands.set('tip', Tip)
 commands.set('bal', Balance)
 commands.set('deposit', DepositRequest)
 commands.set('withdraw', WithdrawalRequest)
+commands.set('whos', Whos)
 
 export function parseCommand (sigil, cmdString) {
   const [cmd, ...args] = cmdString.slice(sigil.length).trim().split(/,? +/g)
---
+++ src/commands/whos.js
@@ -0,0 +1,33 @@
+import { getActiveUsers } from '../activity/index.js'
+import { getCollection } from '../db/index.js'
+
+
+const lf = new Intl.ListFormat('en')
+const ucFirst = (str) =>  str.charAt(0).toUpperCase() + str.slice(1)
+
+export class Whos {
+  static channelTypes = ['GUILD_TEXT']
+  static classifiers = ['active']
+
+  async call (sender, [classifier]) {
+    classifier = classifier.toLowerCase()
+
+    if (!Whos.classifiers.includes(classifier)) {
+      return { message: { body: `Try \`.whos ${Whos.classifiers.join('|')}\``} }
+    }
+
+    const accountsCollection = await getCollection('accounts')
+    const activeUsers = await getActiveUsers(accountsCollection, 30, 30)
+    const activeUsersList = lf.format(activeUsers.map(({ _id }) => `<@${_id}>`))
+
+    const heading = `${ucFirst(classifier)} users`
+    const body = activeUsers.length ? activeUsersList : `No ${classifier} users found`
+
+    return {
+      message: {
+        heading,
+        body
+      }
+    }
+  }
+}
