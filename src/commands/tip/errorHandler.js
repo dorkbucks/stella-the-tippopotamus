10b18b77c02a00cffb4d9fc11170ae5e0690cb0c
Rubber T. Dork <hi@rubberdork.com>
2022-02-07 21:10:49 +0800

    Refactor tip/errorHandler fn

---
+++ src/commands/tip/errorHandler.js
@@ -0,0 +1,14 @@
+import { MessageEmbed } from 'discord.js'
+
+import { logger } from '../../lib/logger.js'
+
+
+export function errorHandler (message) {
+  return function(error) {
+    logger.error(error)
+    const embed = new MessageEmbed()
+      .setColor('#ff2a00')
+      .setDescription(error.message)
+    message.reply({ embeds: [embed] })
+  }
+}
