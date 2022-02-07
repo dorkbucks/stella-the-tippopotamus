fa7798e56203ecabce650957d256482b568046d4
Rubber T. Dork <hi@rubberdork.com>
2022-02-07 21:16:38 +0800

    Export composed tip fns w/ error handler

---
+++ src/commands/tip/index.js
@@ -0,0 +1,23 @@
+import { composeAsync } from '../../lib/composeAsync.js'
+import { extractArgsFromMessage } from './extractArgsFromMessage.js'
+import { parseCommandArgs } from './parseCommandArgs.js'
+import { transformAndValidateToken } from './transformAndValidateToken.js'
+import { transformAndValidateClassifiers } from './transformAndValidateClassifiers.js'
+import { transformAndValidateAccounts } from './transformAndValidateAccounts.js'
+import { transformAndValidateAmount } from './transformAndValidateAmount.js'
+import { updateAccountBalances } from './updateAccountBalances.js'
+import { sendSuccessMessage } from './sendSuccessMessage.js'
+import { errorHandler } from './errorHandler.js'
+
+
+const tip = composeAsync(
+  extractArgsFromMessage,
+  parseCommandArgs,
+  transformAndValidateToken,
+  transformAndValidateClassifiers,
+  transformAndValidateAccounts,
+  transformAndValidateAmount,
+  updateAccountBalances,
+  sendSuccessMessage
+)
+export const execute = (message) => tip(message).catch(errorHandler(message))
