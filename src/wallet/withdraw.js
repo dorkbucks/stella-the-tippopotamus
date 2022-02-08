cb1f296790fd3792fe94d3cca693da491a69fdc6
Rubber T. Dork <hi@rubberdork.com>
2022-02-08 14:29:53 +0800

    Put wallet/withdraw() back into a queue

--- src/wallet/withdraw.js
+++ src/wallet/withdraw.js
@@ -7,12 +7,12 @@ import { Queue } from '../lib/queue.js'
 const queue = new Queue()
 const workerFile = new URL('./withdraw_worker.js', import.meta.url)
 
-export function withdraw (account, { amount, token, address, memo }) {
-  return new Promise((resolve, reject) => {
+export function withdraw(account, { amount, token, address, memo }) {
+  return queue.add(() => new Promise((resolve, reject) => {
     const worker = new Worker(workerFile, {
       workerData: { account, amount, token, address, memo }
     })
     worker.on('message', resolve)
     worker.on('error', reject)
-  })
+  }))
 }
