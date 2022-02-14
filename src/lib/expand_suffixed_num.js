5e75b44e14a65e5f8594b05a2617ea068012d497
Rubber T. Dork <hi@rubberdork.com>
2022-02-14 10:42:04 +0800

    Move const out of fn

--- src/lib/expand_suffixed_num.js
+++ src/lib/expand_suffixed_num.js
@@ -1,10 +1,11 @@
+const multiplier = {
+  k: 1000,
+  m: 1000000,
+  b: 1000000000
+}
+
 export function expandSuffixedNum (num) {
   const [n, suffix] = num.split(/(?=[k|m|b])/i)
   if (!suffix) return parseFloat(n)
-  const multiplier = {
-    k: 1000,
-    m: 1000000,
-    b: 1000000000
-  }
   return parseFloat(n) * multiplier[suffix.toLowerCase()]
 }
