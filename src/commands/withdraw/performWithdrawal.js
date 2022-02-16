dfcb56a7df8c551d6f09e40553bc7de6d2db27ff
Rubber T. Dork <hi@rubberdork.com>
2022-02-16 17:20:24 +0800

    Catch errors from withdrawals

--- src/commands/withdraw/performWithdrawal.js
+++ src/commands/withdraw/performWithdrawal.js
@@ -1,17 +1,22 @@
 import { MessageEmbed } from 'discord.js'
 
 import { expertTxnURL } from '../../stellar/index.js'
+import { errorHandler } from '../errorHandler.js'
 
 
-export async function performWithdrawal (args) {
-  args.sender.withdraw(args).then(({ transactionHash }) => {
-    const txLink = expertTxnURL(transactionHash)
-    const embed = new MessageEmbed()
-      .setColor('#ff9900')
-      .setAuthor(`${args.token.name} withdrawal successful`)
-      .setDescription(`[View the transaction on Stellar Expert](${txLink})`)
-    return args.message.author.send({ embeds: [embed] })
-  })
+export async function performWithdrawal(args) {
+  args.sender.withdraw(args)
+    .then(({ transactionHash }) => {
+      const txLink = expertTxnURL(transactionHash)
+      const embed = new MessageEmbed()
+        .setColor('#ff9900')
+        .setAuthor(`${args.token.name} withdrawal successful`)
+        .setDescription(`[View the transaction on Stellar Expert](${txLink})`)
+      return args.message.author.send({ embeds: [embed] })
+    })
+    // Errors from the worker thread don't bubble up to the composed execute
+    // function's catch so we catch it here.
+    .catch(errorHandler(args.message))
 
   const embed = new MessageEmbed()
     .setColor('#ff9900')
